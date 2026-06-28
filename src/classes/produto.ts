a6b5220a9fc235710a5405ed0e4f32dc2532a792
Mateus <mateus12jesusteteu@gmail.com>
2026-06-27 23:28:10 -0300

    Estrutura inicial e teste.
    Estrutura inicial do projeto configurada e organizada. Implementada a classe Produto e realizados testes básicos para validar o funcionamento e a integridade da lógica.

---
+++ src/classes/produto.ts
@@ -0,0 +1,11 @@
+export class Produto {
+    nome:string
+    preco:number
+    quantidade:number
+
+    constructor(nome:string, preco:number, quantidade:number){
+        this.nome = nome
+        this.preco = preco
+        this.quantidade = quantidade
+    }
+}
---
+++ src/script.ts
@@ -0,0 +1,3 @@
+let nome:string = String(prompt("Informe seu nome: "))
+alert(`Teste bem sucedido ${nome}`)
+
